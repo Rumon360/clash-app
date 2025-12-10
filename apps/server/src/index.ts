a80f74e6f95aae4dd4980d5d37511c0c27e72190
Rumon360 <hmk.rumon@gmail.com>
2025-12-11 00:47:05 +0600

    chore: ejs setup

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,23 +1,35 @@
 import "dotenv/config";
 import cors from "cors";
 import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 
 app.use(
-	cors({
-		origin: process.env.CORS_ORIGIN || "",
-		methods: ["GET", "POST", "OPTIONS"],
-	}),
+  cors({
+    origin: process.env.CORS_ORIGIN || "",
+    methods: ["GET", "POST", "OPTIONS"],
+  })
 );
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.set("view engine", "ejs");
+app.set("views", path.resolve(__dirname, "./views"));
 
 app.get("/", (_req, res) => {
-	res.status(200).send("OK");
+  res.status(200).send("OK");
+});
+
+app.get("/welcome", (_req, res) => {
+  res.render("welcome");
 });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-	console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
