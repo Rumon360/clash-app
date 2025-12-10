88b4fd40de767ec6dbb82aa5003f2a306d1d09ad
Rumon360 <hmk.rumon@gmail.com>
2025-12-10 23:09:31 +0600

    chore: user table

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -2,8 +2,12 @@ import { PrismaClient } from "../prisma/generated/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
 const adapter = new PrismaPg({
-	connectionString: process.env.DATABASE_URL || "",
+  connectionString: process.env.DATABASE_URL || "",
 });
-const prisma = new PrismaClient({ adapter });
+const prisma = new PrismaClient({
+  log: ["error", "query"],
+  errorFormat: "pretty",
+  adapter,
+});
 
 export default prisma;
