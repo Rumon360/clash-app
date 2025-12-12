48f2f5a046cf8677b27ec502c07e78b4cb4ce6f5
Rumon360 <hmk.rumon@gmail.com>
2025-12-13 00:35:51 +0600

    chore: extra

--- packages/mail/src/index.ts
+++ packages/mail/src/index.ts
@@ -1,19 +1,32 @@
-import nodemailer from "nodemailer";
+import path from "path";
+import { fileURLToPath } from "url";
+import dotenv from "dotenv";
+import nodemailer, { type Transporter } from "nodemailer";
+import { MailtrapTransport } from "mailtrap";
 
-export const transporter = nodemailer.createTransport({
-  host: "smtp.ethereal.email",
-  port: 587,
-  secure: false, // true for 465, false for other ports
-  auth: {
-    user: "maddison53@ethereal.email",
-    pass: "jn7jnAPss4f63QBp6D",
-  },
-});
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+dotenv.config({ path: path.join(__dirname, "../.env") });
+
+const TOKEN = process.env.MAILTRAP_TOKEN!;
+const INBOX_ID = Number(process.env.MAILTRAP_INBOX_ID);
+
+export const transporter: Transporter = nodemailer.createTransport(
+  MailtrapTransport({
+    token: TOKEN,
+    sandbox: true,
+    testInboxId: INBOX_ID,
+  })
+);
 
 export const sendMail = async (to: string, subject: string, body: string) => {
+  const sender = {
+    address: "hello@example.com",
+    name: "Mailtrap Test",
+  };
+
   try {
     await transporter.sendMail({
-      from: process.env.FROM_EMAIL,
+      from: sender,
       to: to,
       subject: subject,
       html: body,
